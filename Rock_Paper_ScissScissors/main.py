38705dcbf73e8300b61a07d04a6cf024037c4ba1
brauliobolano <115350502+brauliobolano@users.noreply.github.com>
2023-07-27 19:43:54 -0400

    Added comments main.py
    Added Comments

--- Rock_Paper_ScissScissors/main.py
+++ Rock_Paper_ScissScissors/main.py
@@ -3,9 +3,13 @@ import random
 
 
 def play():
+    # Get user's choice
     user_choice = input(" Select one choice: 'r' for rock, 'p' for paper, 's' for scissor: ")
+    
+    # Generate computer's choice randomly
     computer_choice = random.choice(['r', 'p', 's'])
-
+    
+     # Determine the winner or if it's a tie
     if user_choice == computer_choice:
         return "Tie"
     elif win(user_choice, computer_choice):
@@ -17,6 +21,7 @@ def play():
 
 
 def win(player, opponent):
+    # Check if the player wins based on the rules: r > s, p > r, s > p
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
 
